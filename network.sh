8fe37e5c3354da6778c059a45aadc946e7324e32
Leonardo Lopes <30469023+llopess@users.noreply.github.com>
2024-10-03 23:23:56 -0300

    feat: add script to get ping data

---
+++ network.sh
@@ -0,0 +1,11 @@
+LOG=/Users/leonardo.lopes/Documents/$(date +%s)-network-monitor.log
+
+echo "Script desenvolvido por Leonardo Lopes"
+
+echo "Executando comando 'ping' com $1 envios de pacote às $(date)"
+
+echo "===Inicio===" >> $LOG
+
+ping -c $1 $2 >> $LOG
+
+echo "===Fim de execução===" >> $LOG

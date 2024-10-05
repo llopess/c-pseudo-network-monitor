660e9e60519f2140b6736b0eb7ba45334b39e2a1
Leonardo Lopes <30469023+llopess@users.noreply.github.com>
2024-10-04 22:17:40 -0300

    feat: add print for tracert and ipconfig infos

--- monitor.c
+++ monitor.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 
 void printSeparador(char data[]) {
-	printf("=====%s=====\n", data);
+	printf("=====%s=====\n\n", data);
 }
 
 float verificaMenorTempo(float menorTempo, float tempoDeExecucao) {
-	if(menorTempo < tempoDeExecucao) {
+	if (menorTempo < tempoDeExecucao) {
 		return menorTempo;
 	} else {
 		return tempoDeExecucao;
@@ -13,15 +14,25 @@ float verificaMenorTempo(float menorTempo, float tempoDeExecucao) {
 }
 
 float verificaMaiorTempo(float maiorTempo, float tempoDeExecucao) {
-	if(maiorTempo > tempoDeExecucao) {
+	if (maiorTempo > tempoDeExecucao) {
 		return maiorTempo;
 	} else {
 		return tempoDeExecucao;
 	}
 }
 
-int main() {
+void printIpconfig() {
+    char ipv4[] = "192.168.2.120";
+    char ipv6[] = "2804:60:367:9c00:e6cb:b67:47aa:9526";
+    
+    printSeparador("Informações IP");
+    printf("Dados coletados a partir da exeucão do comando ipconfig:\n");
+    printf("IPv4: %s\n", ipv4);
+    printf("IPv6: %s\n", ipv6);
+    printSeparador("");
+}
 
+void printPing() {
 	char dadosColetados[3][62] = {
 		"64 bytes from 172.217.29.46: icmp_seq=0 ttl=51 time=27.156 ms",
 		"64 bytes from 172.217.29.46: icmp_seq=1 ttl=51 time=27.259 ms",
@@ -36,11 +47,15 @@ int main() {
 	int pacotesRecebidos = 3;
 	int pacotesTransmitidos = 3;
 	float pacotesPerdidos = 0;
-
-	printSeparador("Monitor de redes do Leonardo");
-
-	printf("Comando 'ping www.google.com.br'\n");
-	printf("Dados coletados:\n");
+	
+	int contador = 0;
+	float menorTempo = 36000;
+	float maiorTempo;
+	float mediaDeTempo;
+	float tempoTotalDeExecucao;
+	
+	printSeparador("Comando 'ping'");
+	printf("Dados coletados a partir da execução do comando 'ping www.google.com.br':\n");
 
 	for (int i = 0; i < array_length; i++) {
 		printf("%s\n", dadosColetados[i]);
@@ -52,12 +67,6 @@ int main() {
 	printf("%d pacotes recebidos\n", pacotesRecebidos);
 	printf("%.2f%% pacotes perdidos\n", pacotesPerdidos);
 
-	int contador = 0;
-	float menorTempo = 36000;
-	float maiorTempo;
-	float mediaDeTempo;
-	float tempoTotalDeExecucao;
-
 	while (contador < array_length) {
 		tempoTotalDeExecucao += temposDeExecucao[contador];
 
@@ -68,12 +77,50 @@ int main() {
 	}
 
 	printSeparador("");
+	
+	printf("Tempo mínimo: %.3fms\n", menorTempo);
+	printf("Tempo máximo: %.3fms\n", maiorTempo);
+	printf("Tempo médio: %.3fms\n", (tempoTotalDeExecucao/array_length));
 	printf("Tempo total: %.3fms\n", tempoTotalDeExecucao);
-	printf("Tempo mC-nimo: %.3fms\n", menorTempo);
-	printf("Tempo mC!ximo: %.3fms\n", maiorTempo);
-	printf("Tempo mC)dio: %.3fms\n", (tempoTotalDeExecucao/array_length));
+	
+	printSeparador("");
+}
 
-	printSeparador("Fim de execuC'C#o");
+void printTracert() {
+    char tracertInfo[13][55] = {
+        "*    	*    	* 	Esgotado o tempo limite do pedido.",
+        "54 ms	31 ms	13 ms  2804:60:6000:171::2",
+        " 5 ms	57 ms	26 ms  2804:60:6000:171::1",
+        "13 ms 	5 ms 	8 ms  fc00::12",
+        "11 ms	22 ms	10 ms  2804:d50:8c:7001::1",
+        "*    	*    	* 	Esgotado o tempo limite do pedido.",
+        "*   	31 ms	29 ms  2001:4860:1:1::f72",
+        "32 ms	32 ms	31 ms  2800:3f0:8073::1",
+        "41 ms	27 ms	31 ms  2001:4860:0:1::2062",
+        "30 ms	33 ms	33 ms  2001:4860:0:1::12e0",
+        "32 ms 	*   	30 ms  2001:4860:0:1::7c85",
+        "34 ms	36 ms	36 ms  2001:4860:0:1::67d1",
+        "30 ms	27 ms	27 ms  2800:3f0:4001:81a::2003"
+    };
+    
+    printSeparador("Comando tracert");
+    printf("Dados coletados a partir do comando 'tracert www.google.com.br':\n\n");
+
+    for(int i = 0; i < 13; i++) {
+        printf("%d %s\n", i+1, tracertInfo[i]);
+    }
+}
+
+int main() {
+	printSeparador("Monitor de redes do Leonardo");
+	
+	printIpconfig();
+	sleep(2);
+	printPing();
+	sleep(2);
+	printTracert();
+
+	printSeparador("Fim de execução");
 
 	return 0;
 }
